09d83491f81707a46fd4ebd04628d0a589c09f8f
tuan nguyen <shevatuan2006@gmail.com>
2024-07-27 21:57:18 +0700

    First try

---
+++ nhtcamera/src/main/java/com/nht/nhtcamera/CameraManager.kt
@@ -0,0 +1,13 @@
+package com.nht.nhtcamera
+
+import android.content.Context
+
+class CameraManager {
+    fun initializeCamera(context: Context) {
+        // Implementation code
+    }
+
+    fun startCamera() {
+        // Implementation code
+    }
+}
